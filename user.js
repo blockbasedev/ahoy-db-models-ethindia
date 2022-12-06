cee99f7f805261b7272d07ca31036806dfb990a7
Chaal Pritam <chaalpritam@gmail.com>
2022-12-06 18:58:12 +0530

    user models change

--- user.js
+++ user.js
@@ -3,8 +3,8 @@ import { Schema, model } from 'mongoose';
 const UserSchema = new Schema(
   {
     walletAddress: { type: String },
+    username: { type: String },
     email: { type: String },
-    password: { type: String },
     verified: { type: Boolean, default: false },
     blocked: { type: Boolean, default: false },
   },
