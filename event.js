6b8b4f83a2cff1dd50e9e4d24ca47d7b91d3a8bf
sathishkumarmsk <sathishkumarmsk5699@gmail.com>
2022-12-07 18:34:20 +0530

    add refealCode feild

--- event.js
+++ event.js
@@ -17,7 +17,7 @@ const EventSchema = new Schema(
     guestCount: { type: Number, default: 0 },
     isActive: { type: Boolean, default: false },
     isDeleted: { type: Boolean, default: false },
-    // referalCode: { type: String, index: true, unique: true },
+    referalCode: { type: String, index: true, unique: true },
     otherLink: { type: String },
   },
   {
