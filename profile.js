3c9262005ac5f06268759431b3422bfe475b8b86
sathishkumarmsk <sathishkumarmsk5699@gmail.com>
2022-12-07 14:17:08 +0530

    profile model add

--- profile.js
+++ profile.js
@@ -7,24 +7,15 @@ const ProfileSchema = new Schema(
     },
     name: { type: String, required: true },
     username: { type: String, required: true },
-    bio: { type: String },
-    tagline: { type: String },
-    followersCount: { type: Number, default: 0 },
-    followingCount: { type: Number, default: 0 },
-    city: { type: String },
-    profilePic: { type: Object },
-    notificationCount: { type: Number, default: 0 },
-    messageCount: { type: Number, default: 0 },
-    shuffleCount: { type: Number, default: 0 },
-    isActive: { type: Boolean, default: false },
-    isDeleted: { type: Boolean, default: false },
-    isCommunity: { type: Boolean, default: false },
-    socialMediaLinks: { type: Object },
-    website: { type: String },
+    about: { type: String },
+    twitterUserName: { type: String },
+    telegram: { type: String },
+    discord: { type: String },
   },
   {
-    timestamps: true,
+      timestamps: true,
   },
+  
 );
 
 export default model('profile', ProfileSchema);
